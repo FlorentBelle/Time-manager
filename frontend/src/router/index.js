a2ede26070d03091a2b5a5074b25f4fcef664991
FlorentBelle <flobel2222@yahoo.fr>
2022-10-28 19:18:43 +0200

    Fixed import issue

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -10,14 +10,15 @@ import Dashboard from '../components/dashboard/Dashboard.vue'
 import All from '../components/dashboard/graphs/All.vue'
 import Bar from '../components/dashboard/graphs/Bar.vue'
 import Line from '../components/dashboard/graphs/Line.vue'
+import Another from '../components/dashboard/graphs/Another.vue'
 // Create a new router instance
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      component: Login,
-    },
+    // {
+    //   path: '/',
+    //   component: Login,
+    // },
     {
       path: '/home',
       component: Home
@@ -35,7 +36,8 @@ const router = createRouter({
       component: WorkingTimeUpdate
     },
     {
-      path: '/chart_manager/:userID',
+      // path: '/chart_manager/:userID',
+      path: '/',
       component: Dashboard,
       children: [
         {
