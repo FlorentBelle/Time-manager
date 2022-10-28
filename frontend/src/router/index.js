bb4329b8f3ef076e44e15a9cc3e46d14b7de1aee
FlorentBelle <flobel2222@yahoo.fr>
2022-10-28 18:54:50 +0200

    add children components in Dashboard route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,8 +5,11 @@ import Profile from '../components/Profile.vue'
 import WorkingTimesList from '../components/WorkingTimesList.vue'
 import WorkingTimeUpdate from '../components/WorkingTimeUpdate.vue'
 import ClockManager from '../components/ClockManager.vue'
-import Dashboard from '../components/Dashboard.vue'
 
+import Dashboard from '../components/dashboard/Dashboard.vue'
+import All from '../components/dashboard/graphs/All.vue'
+import Bar from '../components/dashboard/graphs/Bar.vue'
+import Line from '../components/dashboard/graphs/Line.vue'
 // Create a new router instance
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -33,7 +36,25 @@ const router = createRouter({
     },
     {
       path: '/chart_manager/:userID',
-      component: Dashboard
+      component: Dashboard,
+      children: [
+        {
+          path: '', 
+          component: All
+        },
+        {
+          path: 'bar', 
+          component: Bar
+        },
+        {
+          path: 'line', 
+          component: Line
+        },
+        {
+          path: 'another', 
+          component: Another
+        },
+      ]
     },
     {
       path: '/users/:userID',
